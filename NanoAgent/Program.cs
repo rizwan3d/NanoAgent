cd3bac1d9307782379aeb02bc83a0bb69df6cbac
Muhammad Rizwan <allgame45@hotmail.com>
2026-04-10 14:50:06 +0500

    Add project files.

---
+++ NanoAgent/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace NanoAgent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+            }
+        }
+    }
+}
