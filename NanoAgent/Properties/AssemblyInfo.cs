a3324f2f7a28919290ccdcd2eb6f23c54e7041aa
Muhammad Rizwan <allgame45@hotmail.com>
2026-05-01 03:58:49 +0500

    Revert "Move GitHub plugin to dedicated project"
    This reverts commit 61f05e42a94f2cde3db34173827ced8ab1485e7c.

--- NanoAgent/Properties/AssemblyInfo.cs
+++ NanoAgent/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NanoAgent.Tests")]
-[assembly: InternalsVisibleTo("NanoAgent.Plugin.GitHub")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
