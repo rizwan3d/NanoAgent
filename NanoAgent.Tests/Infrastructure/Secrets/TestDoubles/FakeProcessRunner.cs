5b8c59c9dc04871ab4ed683086de09c9a2e086ea
Muhammad Rizwan <allgame45@hotmail.com>
2026-05-01 03:54:55 +0500

    Revert "Support Windows sandboxed terminals"
    This reverts commit 025c73837ff3efcfa3d455b6f7a9ac7ef9f2499a.

--- NanoAgent.Tests/Infrastructure/Secrets/TestDoubles/FakeProcessRunner.cs
+++ NanoAgent.Tests/Infrastructure/Secrets/TestDoubles/FakeProcessRunner.cs
@@ -4,17 +4,9 @@ namespace NanoAgent.Tests.Infrastructure.Secrets.TestDoubles;
 
 internal sealed class FakeProcessRunner : IProcessRunner
 {
-    private readonly Queue<IBackgroundProcess> _backgroundProcesses = new();
     private readonly Queue<ProcessExecutionResult> _results = new();
 
-    public List<ProcessExecutionRequest> BackgroundRequests { get; } = [];
-
     public List<ProcessExecutionRequest> Requests { get; } = [];
-
-    public void EnqueueBackgroundProcess(IBackgroundProcess process)
-    {
-        _backgroundProcesses.Enqueue(process);
-    }
 
     public void EnqueueResult(ProcessExecutionResult result)
     {
@@ -34,63 +26,5 @@ internal sealed class FakeProcessRunner : IProcessRunner
         }
 
         return Task.FromResult(_results.Dequeue());
-    }
-
-    public IBackgroundProcess StartBackground(
-        ProcessExecutionRequest request,
-        CancellationToken cancellationToken)
-    {
-        cancellationToken.ThrowIfCancellationRequested();
-        BackgroundRequests.Add(request);
-
-        if (_backgroundProcesses.Count == 0)
-        {
-            throw new InvalidOperationException("No queued background process is available.");
-        }
-
-        return _backgroundProcesses.Dequeue();
-    }
-}
-
-internal sealed class FakeBackgroundProcess : IBackgroundProcess
-{
-    private bool _hasExited;
-
-    public FakeBackgroundProcess(
-        string standardOutput = "",
-        string standardError = "",
-        int exitCode = 0)
-    {
-        StandardOutput = new StringReader(standardOutput);
-        StandardError = new StringReader(standardError);
-        ExitCode = exitCode;
-    }
-
-    public TextReader StandardOutput { get; }
-
-    public TextReader StandardError { get; }
-
-    public bool HasExited => _hasExited;
-
-    public int ExitCode { get; }
-
-    public Task StopAsync(CancellationToken cancellationToken)
-    {
-        cancellationToken.ThrowIfCancellationRequested();
-        _hasExited = true;
-        return Task.CompletedTask;
-    }
-
-    public Task WaitForExitAsync(CancellationToken cancellationToken)
-    {
-        cancellationToken.ThrowIfCancellationRequested();
-        _hasExited = true;
-        return Task.CompletedTask;
-    }
-
-    public void Dispose()
-    {
-        StandardOutput.Dispose();
-        StandardError.Dispose();
     }
 }
--- NanoAgent/Infrastructure/Secrets/IProcessRunner.cs
+++ NanoAgent/Infrastructure/Secrets/IProcessRunner.cs
@@ -5,11 +5,4 @@ internal interface IProcessRunner
     Task<ProcessExecutionResult> RunAsync(
         ProcessExecutionRequest request,
         CancellationToken cancellationToken);
-
-    IBackgroundProcess StartBackground(
-        ProcessExecutionRequest request,
-        CancellationToken cancellationToken)
-    {
-        throw new NotSupportedException("Background process execution is not supported.");
-    }
 }
