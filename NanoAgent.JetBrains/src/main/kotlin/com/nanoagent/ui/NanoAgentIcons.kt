8896aefbde69e5068e33405bad5e53cdb3558a3c
Muhammad Rizwan <allgame45@hotmail.com>
2026-05-08 17:54:42 +0500

    Add: Logo

--- NanoAgent.JetBrains/src/main/kotlin/com/nanoagent/ui/NanoAgentIcons.kt
+++ NanoAgent.JetBrains/src/main/kotlin/com/nanoagent/ui/NanoAgentIcons.kt
@@ -7,5 +7,5 @@ import javax.swing.Icon
  * Icon references for the NanoAgent plugin.
  */
 object NanoAgentIcons {
-    val ToolWindow: Icon = IconLoader.getIcon("/icons/nanoagent.svg", NanoAgentIcons::class.java)
+    val ToolWindow: Icon = IconLoader.getIcon("/icons/nanoagent.png", NanoAgentIcons::class.java)
 }
