7f403038c95beaaffc7c32d07df1def8c5e1f688
Muhammad Rizwan <allgame45@hotmail.com>
2026-05-08 17:11:34 +0500

    Add: JetBrains

---
+++ NanoAgent.JetBrains/settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories {
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "NanoAgent.JetBrains"
