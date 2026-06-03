7c660d2e43cb4778e546259a3b8a5ec76317d8b8
rizwan3d <allgame45@hotmail.com>
2026-06-04 03:31:19 +0500

    Fix CLI live layout overflow on small terminals

--- NanoAgent.Tests/CLI/ProgramTests.cs
+++ NanoAgent.Tests/CLI/ProgramTests.cs
@@ -3,6 +3,7 @@ using NanoAgent.CLI;
 using NanoAgent.Application.Backend;
 using Moq;
 using NanoAgent.Infrastructure.WindowsSandbox;
+using Spectre.Console;
 using System.Reflection;
 
 namespace NanoAgent.Tests.CLI;
@@ -63,5 +64,47 @@ public sealed class ProgramTests
 
         state.IsPlanPinned.Should().BeFalse();
         state.LatestPlanText.Should().BeNull();
+    }
+
+    [Fact]
+    public void BuildUi_Should_FallBackToCompactPanel_When_TerminalIsTooSmall()
+    {
+        AppState state = new(
+            new UiBridge(),
+            new Mock<INanoAgentBackend>(MockBehavior.Strict).Object);
+
+        MethodInfo buildUi = typeof(Program).GetMethod(
+            "BuildUi",
+            BindingFlags.NonPublic | BindingFlags.Static,
+            binder: null,
+            [typeof(AppState), typeof(int), typeof(int)],
+            modifiers: null)!;
+
+        object? renderable = buildUi.Invoke(null, [state, 18, 9]);
+
+        renderable.Should().BeOfType<Panel>();
+    }
+
+    [Fact]
+    public void BuildUi_Should_FallBackToCompactPanel_When_PinnedPlanCannotFit()
+    {
+        AppState state = new(
+            new UiBridge(),
+            new Mock<INanoAgentBackend>(MockBehavior.Strict).Object)
+        {
+            IsPlanPinned = true,
+            LatestPlanText = "Plan progress: 1/3"
+        };
+
+        MethodInfo buildUi = typeof(Program).GetMethod(
+            "BuildUi",
+            BindingFlags.NonPublic | BindingFlags.Static,
+            binder: null,
+            [typeof(AppState), typeof(int), typeof(int)],
+            modifiers: null)!;
+
+        object? renderable = buildUi.Invoke(null, [state, 80, 12]);
+
+        renderable.Should().BeOfType<Panel>();
     }
 }
